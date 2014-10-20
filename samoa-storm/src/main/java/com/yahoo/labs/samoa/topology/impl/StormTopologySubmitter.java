fe43586fcbe474ea118c76636f5d4c5024298c25
Andrew Wu <awu@innometrics.com>
2014-10-20 16:02:21 +0200

    upgrade to use storm 0.9.2

--- samoa-storm/src/main/java/com/yahoo/labs/samoa/topology/impl/StormTopologySubmitter.java
+++ samoa-storm/src/main/java/com/yahoo/labs/samoa/topology/impl/StormTopologySubmitter.java
@@ -20,25 +20,16 @@ package com.yahoo.labs.samoa.topology.impl;
  * #L%
  */
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-
-import org.apache.thrift7.TException;
-import org.json.simple.JSONValue;
+import backtype.storm.Config;
+import backtype.storm.StormSubmitter;
+import backtype.storm.generated.AlreadyAliveException;
+import backtype.storm.generated.InvalidTopologyException;
+import backtype.storm.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import backtype.storm.Config;
-import backtype.storm.generated.AlreadyAliveException;
-import backtype.storm.generated.InvalidTopologyException;
-import backtype.storm.utils.NimbusClient;
-import backtype.storm.utils.Utils;
+import java.io.IOException;
+import java.util.*;
 
 /**
  * Helper class to submit SAMOA task into Storm without the need of submitting the jar file.
@@ -47,24 +38,24 @@ import backtype.storm.utils.Utils;
  *
  */
 public class StormTopologySubmitter {
-	
+
 	public static String YJP_OPTIONS_KEY="YjpOptions";
-	
+
 	private static Logger logger = LoggerFactory.getLogger(StormTopologySubmitter.class);
-		
+
 	public static void main(String[] args) throws IOException{
 		Properties props = StormSamoaUtils.getProperties();
-		
+
 		String uploadedJarLocation = props.getProperty(StormJarSubmitter.UPLOADED_JAR_LOCATION_KEY);
 		if(uploadedJarLocation == null){
-			logger.error("Invalid properties file. It must have key {}", 
+			logger.error("Invalid properties file. It must have key {}",
 					StormJarSubmitter.UPLOADED_JAR_LOCATION_KEY);
 			return;
 		}
-		
-		List<String> tmpArgs = new ArrayList<String>(Arrays.asList(args));
+
+		List<String> tmpArgs = new ArrayList<>(Arrays.asList(args));
 		int numWorkers = StormSamoaUtils.numWorkers(tmpArgs);
-		
+
 		args = tmpArgs.toArray(new String[0]);
 		StormTopology stormTopo = StormSamoaUtils.argsToTopology(args);
 
@@ -73,43 +64,26 @@ public class StormTopologySubmitter {
 		conf.putAll(Utils.readCommandLineOpts());
 		conf.setDebug(false);
 		conf.setNumWorkers(numWorkers);
-		
-		String profilerOption = 
+
+		String profilerOption =
 				props.getProperty(StormTopologySubmitter.YJP_OPTIONS_KEY);
 		if(profilerOption != null){
 			String topoWorkerChildOpts =  (String) conf.get(Config.TOPOLOGY_WORKER_CHILDOPTS);
 			StringBuilder optionBuilder = new StringBuilder();
 			if(topoWorkerChildOpts != null){
-				optionBuilder.append(topoWorkerChildOpts);	
+				optionBuilder.append(topoWorkerChildOpts);
 				optionBuilder.append(' ');
 			}
 			optionBuilder.append(profilerOption);
 			conf.put(Config.TOPOLOGY_WORKER_CHILDOPTS, optionBuilder.toString());
 		}
-
-		Map<String, Object> myConfigMap = new HashMap<String, Object>(conf);
-		StringWriter out = new StringWriter();
-
-		try {
-			JSONValue.writeJSONString(myConfigMap, out);
-		} catch (IOException e) {
-			System.out.println("Error in writing JSONString");
-			e.printStackTrace();
-			return;
-		}
-		
 		Config config = new Config();
 		config.putAll(Utils.readStormConfig());
-		
-		String nimbusHost = (String) config.get(Config.NIMBUS_HOST);
-				
-		NimbusClient nc = new NimbusClient(nimbusHost);
 		String topologyName = stormTopo.getTopologyName();
 		try {
-			System.out.println("Submitting topology with name: " 
+			System.out.println("Submitting topology with name: "
 					+ topologyName);
-			nc.getClient().submitTopology(topologyName, uploadedJarLocation,
-					out.toString(), stormTopo.getStormBuilder().createTopology());
+            StormSubmitter.submitTopology(topologyName,config,stormTopo.getStormBuilder().createTopology());
 			System.out.println(topologyName + " is successfully submitted");
 
 		} catch (AlreadyAliveException aae) {
@@ -118,16 +92,6 @@ public class StormTopologySubmitter {
 		} catch (InvalidTopologyException ite) {
 			System.out.println("Invalid topology for " + topologyName);
 			ite.printStackTrace();
-		} catch (TException te) {
-			System.out.println("Texception for " + topologyName);
-			te.printStackTrace();
-		} 		
-	}
-	
-	private static String uploadedJarLocation(List<String> tmpArgs){
-		int position = tmpArgs.size() - 1;
-		String uploadedJarLocation = tmpArgs.get(position);
-		tmpArgs.remove(position);
-		return uploadedJarLocation;
+		}
 	}
 }
